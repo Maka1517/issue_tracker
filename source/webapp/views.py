476f47d7468c0faf1e1e0ef8446d54ba7c8a0eeb
Kurmanbek <kkmambetov@gmail.com>
2020-08-06 18:47:50 +0600

    Добавлена главная страница

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponseNotAllowed
+from django.utils.timezone import make_naive
+from django.views.generic import View, TemplateView
 
-# Create your views here.
+from webapp.models import Issue
+#from webapp.forms import ArticleForm, BROWSER_DATETIME_FORMAT
+
+
+class IndexView(View):
+    def get(self, request):
+        is_admin = request.GET.get('is_admin', None)
+        if is_admin:
+            data = Issue.objects.all()
+        else:
+            data = Issue.objects.all()
+        return render(request, 'index.html', context={
+            'issues': data
+        })
+
+
