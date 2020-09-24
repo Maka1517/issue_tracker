d3c0e905e968c3325bfee56f73422ba3928be207
Kurmanbek <kkmambetov@gmail.com>
2020-09-24 22:43:28 +0600

    добавлен личный кабинет админа с картиной

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import RegisterView
+from .views import RegisterView, UserDetailView
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('accounts/login/', LoginView.as_view(), name='login'),
     path('accounts/logout/', LogoutView.as_view(), name='logout'),
-    path('create/', RegisterView.as_view(), name='create')
+    path('create/', RegisterView.as_view(), name='create'),
+    path('<int:pk>/', UserDetailView.as_view(), name='detail')
 ]
--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,8 +1,9 @@
-from django.contrib.auth import login
+from django.contrib.auth import login, get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse
-from django.views.generic import View, TemplateView, CreateView, UpdateView, DeleteView
+from django.views.generic import View, TemplateView, CreateView, UpdateView, DeleteView, DetailView
 from django.contrib.auth.models import User
 from .forms import MyUserCreationForm
 
@@ -26,19 +27,19 @@ class RegisterView(CreateView):
         return next_url
 
 
-# class UserDetailView(LoginRequiredMixin, DetailView):
-#     model = get_user_model()
-#     template_name = 'user_detail.html'
-#     context_object_name = 'user_obj'
-#     paginate_related_by = 5
-#     paginate_related_orphans = 0
-#
-#     def get_context_data(self, **kwargs):
-#         articles = self.object.articles.order_by('-created_at')
-#         paginator = Paginator(articles, self.paginate_related_by, orphans=self.paginate_related_orphans)
-#         page_number = self.request.GET.get('page', 1)
-#         page = paginator.get_page(page_number)
-#         kwargs['page_obj'] = page
-#         kwargs['issues'] = page.object_list
-#         kwargs['is_paginated'] = page.has_other_pages()
-#         return super().get_context_data(**kwargs)
+class UserDetailView(LoginRequiredMixin, DetailView):
+    model = get_user_model()
+    template_name = 'user_detail.html'
+    context_object_name = 'user_obj'
+    paginate_related_by = 5
+    paginate_related_orphans = 0
+
+    def get_context_data(self, **kwargs):
+        issues = self.object.issues.order_by('-created_at')
+        paginator = Paginator(issues, self.paginate_related_by, orphans=self.paginate_related_orphans)
+        page_number = self.request.GET.get('page', 1)
+        page = paginator.get_page(page_number)
+        kwargs['page_obj'] = page
+        kwargs['issues'] = page.object_list
+        kwargs['is_paginated'] = page.has_other_pages()
+        return super().get_context_data(**kwargs)
